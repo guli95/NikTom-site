322b56a5ad88aae7429e763c245b0f31300ecf9c
Piotr Tratwal <piotr.tratwal@gmail.com>
2022-05-01 13:30:57 +0200

    fix sass

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./css/reset.css";
-import "./css/bgc.css";
 
 ReactDOM.render(
   <React.StrictMode>
